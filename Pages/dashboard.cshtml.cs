626d34b83a6ddf9650400d0528a17c46c3f7db18
muhammed-ayman <mae252002@gmail.com>
2023-05-13 01:48:37 +0300

    feat(admin dashboard): create admin dashboard layout
    • create admin dashboard layout with two initial sections (statistics & support tickets), this should be properly integrated with the back-end privileges of the user type of admin

---
+++ Pages/dashboard.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ScholarStack.Models;
+
+namespace ScholarStack.Pages;
+
+public class DashboardModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
