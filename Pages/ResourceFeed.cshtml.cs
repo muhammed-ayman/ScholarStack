d855db457f780789f7cc309d896a856517f57217
ahmedhesham47 <ahmedheshamgalal47@gmail.com>
2023-05-13 15:52:31 +0300

    feat(resource post page): create resource post page
    • create resource post view
    • integrate resource post view with layout, index, and faqs
    • changed upvotes/downvotes to rating stars

---
+++ Pages/ResourceFeed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScholarStack.Pages
+{
+    public class ResourceFeedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
