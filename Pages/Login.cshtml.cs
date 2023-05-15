169a703d8f4012d206757a583e9f7b3da1f63a13
ahmedhesham47 <ahmedheshamgalal47@gmail.com>
2023-05-15 20:30:04 +0300

    refactor(register): update register form
    • modify registration form
    • create separate on post methods for login and register
    • create general structure for view model

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ScholarStack.Models;
 
 namespace ScholarStack.Pages
 {
     public class LoginModel : PageModel
     {
+        
+        private readonly DB_Manager _dbManager;
+
+        [BindProperty]
+        public User u { get; set; }
+        
+        public LoginModel(DB_Manager db)
+        {
+            _dbManager = db;
+        }
         public void OnGet()
         {
         }
-    }
+
+        public void OnPost() {
+            
+        }
+        
+        public IActionResult OnPostRegister()
+        {
+            
+            return RedirectToPage("Index");
+        }
+
+		public IActionResult OnPostLogin()
+		{
+			return RedirectToPage("Index");
+		}
+	}
 }
