02831fe750211cb5f90a319b4e9d744a6bb4c4d0
ahmedhesham47 <ahmedheshamgalal47@gmail.com>
2023-05-15 20:31:35 +0300

    feat(db_manager): update db_manager functions
    • create general structure for database functions that of the type ExecuteNonQuery
    • link database with user class

--- Models/DB_Manager.cs
+++ Models/DB_Manager.cs
@@ -6,10 +6,48 @@ namespace ScholarStack.Models
 	public class DB_Manager
 	{
 		public SqlConnection con { get; set; }
-		public DB_Manager() 
+		public DB_Manager()
 		{
 			string conString = "Data Source = MOHZMAGDY; Initial Catalog = ScholarStack; Integrated Security = True";
 			con = new SqlConnection(conString);
+		}
+
+		public void AddUser(User u){ // Function should add user to the database
+			string query = "SELECT * FROM USER"; // Only an example
+			FExecuteNonQuery(query);
+		}
+
+		public User UpdateUser(User u) // Function should update the info of user
+			// It might be useful in the Edit Profile page
+		{
+			string query = "SELECT * FROM USER"; // Only an example
+			FExecuteNonQuery(query);
+			return u;
+		}
+
+		public void DeleteUser(User u) // Function should delete the user
+			// It will be helpful for the admins
+		{
+			string query = "SELECT * FROM USER"; // Only an example
+			FExecuteNonQuery(query);
+		}
+
+		private void FExecuteNonQuery(string q) // General function that executes any non query
+			// That is, add, modify, and delete can all be made using this function
+		{
+			try
+			{
+				con.Open();
+				SqlCommand cmd = new SqlCommand(q, con);
+				cmd.ExecuteNonQuery(); // Since it returns "n rows affected".
+				con.Close();
+
+			}
+			catch (Exception ex)
+			{
+				con.Close();
+				// To be edited
+			}
 		}
 	}
 }
