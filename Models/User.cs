6d9f05a0d60068347488534d5d6da39b274c2d84
ahmedhesham47 <ahmedheshamgalal47@gmail.com>
2023-05-15 20:26:17 +0300

    feat(user): create user class
    • create user class with model validation

---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿namespace ScholarStack.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+	public class User
+	{
+		public int ID { get; set; }
+
+		[Required(ErrorMessage = "First Name is Required"), MinLength(2), MaxLength(100)]
+		public string FirstName { get; set; }
+
+		[Required (ErrorMessage = "Last Name is Required"), MinLength(2), MaxLength(100)]
+		public string LastName { get; set; }
+
+		[Required (ErrorMessage = "Email is Required"), EmailAddress]
+		public string Email { get; set; }
+
+		[Required (ErrorMessage = "Password is Required"), PasswordPropertyText]
+		public string Password { get; set; }
+
+		[Required (ErrorMessage = "Username is Required"), MinLength(5), MaxLength(100)]
+		public string UserName { get; set; }
+
+		[Required (ErrorMessage = "Google Scholar Profile URL is Required"), Url]
+		public string GoogleScholarURL { get; set; }
+
+	}
+
