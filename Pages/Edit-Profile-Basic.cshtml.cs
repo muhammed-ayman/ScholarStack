ae580cb690a64167787b75711321afd587c1542d
ahmedhesham47 <ahmedheshamgalal47@gmail.com>
2023-05-20 01:49:28 +0300

    feat(edit-profile): fetch authenticated user's data
    • fetch authenticated user's data from the session key `UserName`
    • personalize the edit profile view for the user

--- Pages/Edit-Profile-Basic.cshtml.cs
+++ Pages/Edit-Profile-Basic.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ScholarStack.Data;
+using ScholarStack.Models;
 
 namespace ScholarStack.Pages
 {
     public class Edit_Profile_BasicModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+
+		private readonly ILogger<IndexModel> _logger;
+		private readonly ScholarStackDBContext _dbContext;
+		public User AuthenticatedUser { get; set; }
+
+		public Edit_Profile_BasicModel(ILogger<IndexModel> logger, ScholarStackDBContext dBContext)
+		{
+			_logger = logger;
+			_dbContext = dBContext;
+		}
+		public void OnGet()
+		{
+			RetrieveAuthenticatedUser();
+		}
+
+		private void RetrieveAuthenticatedUser()
+		{
+			AuthenticatedUser = _dbContext.User.FirstOrDefault(u => u.Username == HttpContext.Session.GetString("UserName"));
+		}
+	}
 }
