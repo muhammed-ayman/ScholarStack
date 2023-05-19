41b4795db1327db2503779200c57b04e9b02d8c2
ahmedhesham47 <ahmedheshamgalal47@gmail.com>
2023-05-20 01:50:20 +0300

    feat(profile): fetch authenticated user's data
    • fetch authenticated user's data from the session key `UserName`
    • personalize the profile view for the user
    • change 'languages' to articles

--- Pages/Profile.cshtml.cs
+++ Pages/Profile.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ScholarStack.Data;
+using ScholarStack.Models;
+using System;
 
 namespace ScholarStack.Pages
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
+		private readonly ILogger<IndexModel> _logger;
+		private readonly ScholarStackDBContext _dbContext;
+		public User AuthenticatedUser { get; set; }
+
+		public ProfileModel(ILogger<IndexModel> logger, ScholarStackDBContext dBContext)
+		{
+			_logger = logger;
+			_dbContext = dBContext;
+		}
+
+		public void OnGet()
         {
-        }
-    }
+			RetrieveAuthenticatedUser();
+		}
+
+		private void RetrieveAuthenticatedUser()
+		{
+			AuthenticatedUser = _dbContext.User.FirstOrDefault(u => u.Username == HttpContext.Session.GetString("UserName"));
+		}
+	}
 }
