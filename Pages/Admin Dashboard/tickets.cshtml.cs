c24809225024777297078a3ca9d6e44f1b7f7c9d
MohzMagdy <moh.magdy.ibrahim@gmail.com>
2023-05-20 16:18:36 +0300

    refactor(dashboard): divide dashboard to multiple pages
    • move `dashboard` to `/Admin Dashboard`
    • create `_admin-shortcuts` partial view
    • move Support Tickets to `tickets`
    • move User Management to `user-management`
    • change Statistics interface
    • change all referencing in `_Layout` to use absolute path

---
+++ Pages/Admin Dashboard/tickets.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScholarStack.Pages.Admin_Dashboard
+{
+    public class ticketsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Admin Dashboard/user-management.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScholarStack.Pages.Admin_Dashboard
+{
+    public class user_managementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
