a685a6f1d19b9224b816948ccd9c46df94639d6c
ahmedhesham47 <ahmedheshamgalal47@gmail.com>
2023-05-20 14:18:35 +0300

    feat(widget): make widget a partial view
    • make the side widget a partial view that can be used throughout the web app
    • add the necessary authentication in all the pages that use the widget

--- Pages/ResourceFeed.cshtml.cs
+++ Pages/ResourceFeed.cshtml.cs
@@ -1,16 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ScholarStack.Data;
+using ScholarStack.Models;
 
 namespace ScholarStack.Pages
 {
     public class ResourceFeedModel : PageModel
     {
 
+		private readonly ILogger<IndexModel> _logger;
+		private readonly ScholarStackDBContext _dbContext;
+
+		public User AuthenticatedUser { get; set; }
+
+		public ResourceFeedModel(ILogger<IndexModel> logger, ScholarStackDBContext dBContext)
+		{
+			_logger = logger;
+			_dbContext = dBContext;
+		}
+
 		public IActionResult OnGetPartial() =>
 	        Partial("_community-post");
 
 		public void OnGet()
         {
-        }
-    }
+			RetrieveAuthenticatedUser();
+		}
+
+		private void RetrieveAuthenticatedUser()
+		{
+			AuthenticatedUser = _dbContext.User.FirstOrDefault(u => u.Username == HttpContext.Session.GetString("UserName"));
+		}
+	}
 }
--- Pages/edit-password.cshtml.cs
+++ Pages/edit-password.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ScholarStack.Data;
+using ScholarStack.Models;
 
 namespace ScholarStack.Pages
 {
     public class edit_passwordModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+
+		private readonly ILogger<IndexModel> _logger;
+		private readonly ScholarStackDBContext _dbContext;
+
+		public User AuthenticatedUser { get; set; }
+
+		public edit_passwordModel(ILogger<IndexModel> logger, ScholarStackDBContext dBContext)
+		{
+			_logger = logger;
+			_dbContext = dBContext;
+		}
+		public void OnGet()
+		{
+			RetrieveAuthenticatedUser();
+		}
+
+		private void RetrieveAuthenticatedUser()
+		{
+			AuthenticatedUser = _dbContext.User.FirstOrDefault(u => u.Username == HttpContext.Session.GetString("UserName"));
+		}
+	}
 }
