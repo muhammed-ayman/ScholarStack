36cdc2a7a99b076770c74cd9d8c25f3255d17dbe
MohzMagdy <moh.magdy.ibrahim@gmail.com>
2023-05-24 03:34:32 +0300

    feat(user management): implement user ban and activation
    • add ban and activation features in `UserService`, `UserManagementController`, `user-manager`
    • remove debugging code from `user-manager`
    • remove useless code from `user-management.cshtml.cs`

--- Controllers/UserManagementController.cs
+++ Controllers/UserManagementController.cs
@@ -50,5 +50,61 @@ namespace ScholarStack.Controllers
             }
 		}
 
-    }
+		[HttpPost("ban-user")]
+		public IActionResult banUser()
+		{
+			// Get the logged-in user ID from session
+			var userIdString = HttpContext.Session.GetString("UserId");
+			if (!int.TryParse(userIdString, out int userId))
+			{
+				// Unable to parse the user ID, return an unauthorized response
+				return Unauthorized(new { message = "Unauthorized: You're not authorized to access this endpoint!" });
+			}
+
+			int user_id;
+			if (!int.TryParse(Request.Form["user_id"], out user_id))
+			{
+				return BadRequest(new { message = "Invalid User." });
+			}
+
+			bool isBanned = _userService.Ban(user_id);
+			if (isBanned)
+			{
+				return Ok(new { message = "Ban successful" });
+			}
+			else
+			{
+				return BadRequest(new { message = "Unable to ban user." });
+			}
+		}
+
+		[HttpPost("activate-user")]
+		public IActionResult activateUser()
+		{
+			// Get the logged-in user ID from session
+			var userIdString = HttpContext.Session.GetString("UserId");
+			if (!int.TryParse(userIdString, out int userId))
+			{
+				// Unable to parse the user ID, return an unauthorized response
+				return Unauthorized(new { message = "Unauthorized: You're not authorized to access this endpoint!" });
+			}
+
+			int user_id;
+			if (!int.TryParse(Request.Form["user_id"], out user_id))
+			{
+				return BadRequest(new { message = "Invalid User." });
+			}
+
+			bool isActivated = _userService.Activate(user_id);
+			if (isActivated)
+			{
+				return Ok(new { message = "Activation successful" });
+			}
+			else
+			{
+				return BadRequest(new { message = "Unable to activate user." });
+			}
+		}
+
+	}
 }
--- wwwroot/js/dashboard/user-manager.js
+++ wwwroot/js/dashboard/user-manager.js
@@ -1,66 +1,105 @@
 ﻿$(document).ready(function () {
     var userId = -1;
-    // Event handler for clicking the delete button
+
+    // --- User Deletion --- //
+    // Show Delete Modal
     $('table tbody').on('click', 'button.delete-user', function () {
         userId = $(this).closest('tr').data('user-id');
-        console.log(userId);
         $('#deleteUserModal').modal('show');
-
     });
 
+    // Modal Delete button
     $('.delete-user-modal').on('click', function () {
-        var tempUserId = userId;
-        console.log(tempUserId);
         // Perform the POST request to the server
-        $.post('/api/dashboard/delete-user', { user_id: tempUserId })
+        $.post('/api/dashboard/delete-user', { user_id: userId })
             .done(function (data) {
                 // Handle the response from the server
                 console.log(data);
                 successfulDeletion();
             })
             .fail(function (error) {
-                console.log('Delete user request failed', error.responseText);
-                
+                console.log('Delete user request failed');
             });
     });
 
+    // Successful Deletion alert
     function successfulDeletion() {
-        deletedUserEntry = $(`tr[data-user-id="${userId}"]`);
-        deletedUserEntry.remove();
+        UserEntry = $(`tr[data-user-id="${userId}"]`);
+        UserEntry.remove();
         $('#deleteUserModal').modal('hide');
         alert('User deleted successfully!');
     }
 
+
+
+    // --- User Ban --- //
+    // Show Ban Modal
+    $('table tbody').on('click', 'button.ban-user', function () {
+        userId = $(this).closest('tr').data('user-id');
+        $('#banUserModal').modal('show');
+    });
+
+    // Modal Ban button
+    $('.ban-user-modal').on('click', function () {
+        // Perform the POST request to the server
+        $.post('/api/dashboard/ban-user', { user_id: userId })
+            .done(function (data) {
+                // Handle the response from the server
+                console.log(data);
+                successfulBan();
+            })
+            .fail(function (error) {
+                console.log('Ban user request failed');
+            });
+    });
+
+    // Successful Ban alert
+    function successfulBan() {
+        UserEntry = $(`tr[data-user-id="${userId}"]`);
+        UserEntry.css('color', 'red');
+        $('#banUserModal').modal('hide');
+        alert('User banned successfully!');
+    }
+
+
+
+    // --- User Activation --- //
+    // Show Activate Modal
+    $('table tbody').on('click', 'button.activate-user', function () {
+        userId = $(this).closest('tr').data('user-id');
+        $('#activateUserModal').modal('show');
+    });
+
+    // Modal Activate button
+    $('.activate-user-modal').on('click', function () {
+        // Perform the POST request to the server
+        $.post('/api/dashboard/activate-user', { user_id: userId })
+            .done(function (data) {
+                // Handle the response from the server
+                console.log(data);
+                successfulActivate();
+            })
+            .fail(function (error) {
+                console.log('Activate user request failed');
+            });
+    });
+
+    // Successful Activate alert
+    function successfulActivate() {
+        UserEntry = $(`tr[data-user-id="${userId}"]`);
+        UserEntry.css('color', 'black');
+        $('#activateUserModal').modal('hide');
+        alert('User activated successfully!');
+    }
+
+
+
+    // Modal Cancel button
     $(document).on('click', '.modal-cancel', function () {
         // Get the parent modal element
         var parentModal = $(this).closest('.modal');
         // Hide the modal
         parentModal.modal('hide');
     });
-
-    // Event handler for clicking the ban button
-    $('table tbody').on('click', 'button.btn-warning', function () {
-        const userId = $(this).closest('tr').data('userid');
-        $('#banUserModal').data('userid', userId);
-        $('#banUserModal').modal('show');
-    });
-
-    // Event handler for clicking the activate button
-    $('table tbody').on('click', 'button.btn-success', function () {
-        const userId = $(this).closest('tr').data('userid');
-        $('#activateUserModal').data('userid', userId);
-        $('#activateUserModal').modal('show');
-    });
-
-    // Event handler for clicking the ban button in the ban user modal
-    $('#banUserButton').click(function () {
-        const userId = $('#banUserModal').data('userid');
-        banUser(userId);
-    });
-
-    // Event handler for clicking the activate button in the activate user modal
-    $('#activateUserButton').click(function () {
-        const userId = $('#activateUserModal').data('userid');
-        activateUser(userId);
-    });
 });
+
