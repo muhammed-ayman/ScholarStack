b60d352f6e60315ef546b290bc3575b64b7f8a49
muhammed-ayman <mae252002@gmail.com>
2023-05-24 04:51:09 +0300

    feat(cp comments): create CP comments backend API
    • create API endpoint `/api/community-post/{{post-id}}/add-comment`
    • integrate the API endpoint with community post component

--- wwwroot/js/community-post.js
+++ wwwroot/js/community-post.js
@@ -1,71 +1,109 @@
 $(document).ready(function () {
-  // Function to update the vote count
-  function updateVoteCount(postId) {
-      $.ajax({
-          type: 'GET',
-          url: `/api/community-post/${postId}/votes`,
-          success: function (response) {
-              var upvotesElement = $(`.user-post[data-post-id="${postId}"] .upvote-btn ins`);
-              var downvotesElement = $(`.user-post[data-post-id="${postId}"] .downvote-btn ins`);
-              upvotesElement.text(response.upvotes);
-              downvotesElement.text(response.downvotes);
+    // Function to update the vote count
+    function updateVoteCount(postId) {
+        $.ajax({
+            type: 'GET',
+            url: `/api/community-post/${postId}/votes`,
+            success: function (response) {
+                // Update the vote count elements
+                var upvotesElement = $(`.user-post[data-post-id="${postId}"] .upvote-btn ins`);
+                var downvotesElement = $(`.user-post[data-post-id="${postId}"] .downvote-btn ins`);
+                upvotesElement.text(response.upvotes);
+                downvotesElement.text(response.downvotes);
 
-              // Update the vote count in the post meta-data
-              var postMetaElement = $(`.user-post[data-post-id="${postId}"] .post-meta`);
-              var commentCountElement = postMetaElement.find('.comment ins');
-              var upvoteCountElement = postMetaElement.find('.like ins');
-              var downvoteCountElement = postMetaElement.find('.dislike ins');
+                // Update the vote count in the post meta-data
+                var postMetaElement = $(`.user-post[data-post-id="${postId}"] .post-meta`);
+                var commentCountElement = postMetaElement.find('.comment ins');
+                var upvoteCountElement = postMetaElement.find('.like ins');
+                var downvoteCountElement = postMetaElement.find('.dislike ins');
 
-              commentCountElement.text(response.commentCount);
-              upvoteCountElement.text(response.upvotes);
-              downvoteCountElement.text(response.downvotes);
-          },
-          error: function () {
-              // Display an error message if the AJAX request fails
-              alert('An error occurred while retrieving the vote count. Please try again.');
-          }
-      });
-  }
+                commentCountElement.text(response.commentCount);
+                upvoteCountElement.text(response.upvotes);
+                downvoteCountElement.text(response.downvotes);
+            },
+            error: function () {
+                // Display an error message if the AJAX request fails
+                alert('An error occurred while retrieving the vote count. Please try again.');
+            }
+        });
+    }
 
-  // Get all the post elements
-  const postElements = document.querySelectorAll('.user-post');
+    // Function to handle adding a comment
+    function addComment(postId, commentText) {
+        $.ajax({
+            type: 'POST',
+            url: `/api/community-post/${postId}/add-comment`,
+            data: { comment_text: commentText },
+            success: function (response) {
+                // Handle the response from the server
+                console.log('Comment added successfully');
+                console.log(response);
 
-  // Apply the vote handler to each post element
-  postElements.forEach((postElement) => {
-      const postId = postElement.getAttribute('data-post-id');
-      const upvoteButton = postElement.querySelector('.like');
-      const downvoteButton = postElement.querySelector('.dislike');
+                // Optionally, you can perform additional actions after adding the comment
+                // For example, you can update the comment count or display the new comment on the page
 
-      // Add event listeners to the upvote and downvote buttons
-      upvoteButton.addEventListener('click', () => {
-          // Perform the POST request to the server
-          $.post('/api/community-post/upvote', { community_post_id: postId })
-              .done(function (data) {
-                  // Handle the response from the server
-                  console.log('Upvote request successful');
-                  console.log(data);
-                  // Update the vote count
-                  updateVoteCount(postId);
-              })
-              .fail(function (jqXHR, textStatus, errorThrown) {
-                  console.log('Upvote request failed');
-                  console.log('Error:', jqXHR.responseText);
-              });
-      });
+                // Update the vote count
+                updateVoteCount(postId);
+            },
+            error: function () {
+                // Display an error message if the AJAX request fails
+                alert('An error occurred while adding the comment. Please try again.');
+            }
+        });
+    }
 
-      downvoteButton.addEventListener('click', () => {
-          // Perform the POST request to the server
-          $.post('/api/community-post/downvote', { community_post_id: postId })
-              .done(function (data) {
-                  // Handle the response from the server
-                  console.log('Downvote request successful');
-                  console.log(data);
-                  // Update the vote count
-                  updateVoteCount(postId);
-              })
-              .fail(function () {
-                  console.log('Downvote request failed');
-              });
-      });
-  });
+    // Get all the post elements
+    const postElements = document.querySelectorAll('.user-post');
+
+    // Apply the vote handler to each post element
+    postElements.forEach((postElement) => {
+        const postId = postElement.getAttribute('data-post-id');
+        const upvoteButton = postElement.querySelector('.like');
+        const downvoteButton = postElement.querySelector('.dislike');
+        const addCommentButton = postElement.querySelector('.add-comment-btn');
+        const commentTextInput = postElement.querySelector('.comment-text-input');
+
+        // Add event listeners to the upvote and downvote buttons
+        upvoteButton.addEventListener('click', () => {
+            // Perform the POST request to the server
+            $.post('/api/community-post/upvote', { community_post_id: postId })
+                .done(function (data) {
+                    // Handle the response from the server
+                    console.log('Upvote request successful');
+                    console.log(data);
+                    // Update the vote count
+                    updateVoteCount(postId);
+                })
+                .fail(function (jqXHR, textStatus, errorThrown) {
+                    console.log('Upvote request failed');
+                    console.log('Error:', jqXHR.responseText);
+                });
+        });
+
+        downvoteButton.addEventListener('click', () => {
+            // Perform the POST request to the server
+            $.post('/api/community-post/downvote', { community_post_id: postId })
+                .done(function (data) {
+                    // Handle the response from the server
+                    console.log('Downvote request successful');
+                    console.log(data);
+                    // Update the vote count
+                    updateVoteCount(postId);
+                })
+                .fail(function () {
+                    console.log('Downvote request failed');
+                });
+        });
+
+        addCommentButton.addEventListener('click', () => {
+            const commentText = commentTextInput.value;
+            if (commentText.trim() !== '') {
+                // Perform the POST request to add the comment
+                addComment(postId, commentText);
+
+                // Clear the comment text input
+                commentTextInput.value = '';
+            }
+        });
+    });
 });
